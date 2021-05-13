19f2ef563f57c2af3e42b814c1b8c0e73a53d725
Szczurowsky <70973416+Szczurowsky@users.noreply.github.com>
2021-05-13 23:28:59 +0200

    Added security routing

--- Main/urls.py
+++ Main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, logout, login_view, register_view
+from .views import home, logout, login_view, register_view, security
 
 app_name = 'Main'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', login_view, name='Login'),
     path('register/', register_view, name='Register'),
     path('logout/', logout, name='logout'),
+    path('security/', security, name='security')
 ]
