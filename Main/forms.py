9194419efd2b3fd842ab6eddc8bbd5cb1a6c5ae8
Szczurowsky <70973416+Szczurowsky@users.noreply.github.com>
2021-05-13 09:01:01 +0200

    Added register page in routing

--- Main/forms.py
+++ Main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import User
-from django.contrib.auth import login, authenticate
+from django.core.validators import validate_email, validate_slug
+from django.contrib.auth.password_validation import validate_password
 
 
 class LoginValidate(forms.Form):
@@ -20,3 +21,47 @@ class LoginValidate(forms.Form):
                 raise forms.ValidationError('Username or Password are not valid')
         else:
             raise forms.ValidationError('Username or Password are not valid1')
+
+
+class RegisterValidate(forms.Form):
+    username = forms.CharField(error_messages={'required': 'Username can not be empty'})
+    nickname = forms.CharField(error_messages={'required': 'Nickname can not be empty'})
+    email = forms.CharField(error_messages={'required': 'Email address can not be empty'})
+    password = forms.CharField(error_messages={'required': 'Password can not be empty'})
+    password2 = forms.CharField(error_messages={'required': 'Please retype the password'})
+    tos = forms.BooleanField(required=False)
+
+    def clean(self):
+        username = str(self.cleaned_data.get('username'))
+        nickname = str(self.cleaned_data.get('nickname'))
+        email = str(self.cleaned_data.get('email'))
+        password = str(self.cleaned_data.get('password'))
+        password2 = str(self.cleaned_data.get('password2'))
+        tos = self.cleaned_data.get('tos')
+        try:
+            validate_email(email)
+        except forms.ValidationError:
+            raise forms.ValidationError("Email is not correct")
+        try:
+            validate_slug(username)
+        except forms.ValidationError:
+            raise forms.ValidationError("Username is not correct")
+        try:
+            validate_slug(nickname)
+        except forms.ValidationError:
+            raise forms.ValidationError("Nickname is not correct")
+        try:
+            validate_password(password, self)
+        except forms.ValidationError as error:
+            raise forms.ValidationError(error)
+        if password != password2:
+            raise forms.ValidationError('Passwords are not identical')
+        if tos is False:
+            raise forms.ValidationError('ToS need to be accepted')
+        if User.objects.filter(username=username).count() != 0:
+            raise forms.ValidationError('Username already exists')
+        if User.objects.filter(nickname=nickname).count() != 0:
+            raise forms.ValidationError('Nickname already exists')
+        if User.objects.filter(email=email).count() != 0:
+            raise forms.ValidationError('Email address already exists')
+        return True
--- Main/urls.py
+++ Main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, logout, login_view
+from .views import home, logout, login_view, register_view
 
 app_name = 'Main'
 
 urlpatterns = [
     path('', home, name='Home'),
     path('login/', login_view, name='Login'),
+    path('register/', register_view, name='Register'),
     path('logout/', logout, name='logout'),
 ]
